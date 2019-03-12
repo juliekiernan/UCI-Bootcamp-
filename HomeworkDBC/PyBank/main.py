0efabf5a4e741194f2c8e6d15f1c0d515e0a422e
jak <emailjuliekiernan@gmail.com>
2019-03-12 00:08:42 -0700

    update text file

--- HomeworkDBC/PyBank/main.py
+++ HomeworkDBC/PyBank/main.py
@@ -69,10 +69,10 @@ output = open("output.txt", "w")
 line1 = "Financial Analysis"
 line2 = "---------------------"
 line3 = str(f"Total Months: {str(total_months)}")
-line4 = str(f"Total: ${str(net_pl)}")
+line4 = str(f"Net Total: ${str(net_pl)}")
 line5 = str(f"Average Change: ${str(round(avg_change, 2))}")
 line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+line7 = str(f"Greatest Decrease in Losses: {worst_date} (${str(greatest_decrease)})")
 output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4, line5, line6, line7))
 
 
