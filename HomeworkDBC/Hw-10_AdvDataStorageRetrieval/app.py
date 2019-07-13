c70bb5a46de59439c82fe708de1f04a2e18b85a7
jak <emailjuliekiernan@gmail.com>
2019-07-12 17:45:08 -0700

    files for hw

--- HomeworkDBC/Hw-10_AdvDataStorageRetrieval/app.py
+++ HomeworkDBC/Hw-10_AdvDataStorageRetrieval/app.py
@@ -1,56 +1,84 @@
-from flask import flask, jsonify
+# import datetime as dt
+# import numpy as np
+# import pandas as pd
 
-import sqlalchemy
+# import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-import numpy as np
-import pandas as pd
+from flask import Flask, jsonify
 
-
-engine = create_engine("sqlite:///hawaii.sqlite")
+#set-up connection to database 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
+# Create session (link) to the DB
 session = Session(engine)
 
+#set-up flask
 app = Flask(__name__)
 
+#all available flask routes
 @app.route("/")
 def home():
-    print("Server received request for home page")
     return(
-        f"Homepage for Hawaiian Climate Analysis"<br/>
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<starts>/<end><br/>"
+        f"Homepage for Hawaiian Climate Analysi\n"
+        f"Available Routes:\n"
+        f"/api/v1.0/precipitation\n"
+        f"/api/v1.0/stations\n"
+        f"/api/v1.0/tobs\n"
+        f"/api/v1.0/<start\n"
+        f"/api/v1.0/<starts>\n"
         )
 
+# convert query results to dictionary using date as the key, and prcp as the value
 @app.route("/api/v1.0/precipitation")   
 def precipitation():
-    return jsonify (precipitation):
+    prior_yr = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prior_yr).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
-@app.route("/api/v1.0/stations")   
+# return  json list of stations
+@app.route("/api/v1.0/stations")
 def stations():
-    return jsonify (num_stations):
+    results = session.query(Station.station).all()
+    station = list(np.ravel(results))
+    return jsonify(station)
 
-@app.route("/api/v1.0/tobs")   
+# query for dates & temps from a year from the last data point; return json list of temps for that period
+@app.route("/api/v1.0/tobs")
 def tobs():
-    return jsonify (tobs):
+    prior_yr = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= prior_yr).all()
+    temp = list(np.ravel(results))
+    return jsonify (temp)
 
-@app.route("/api/v1.0/<start>")   
-def start_date():
-    return jsonify (start_date):
+# return a josn list of temp min, max and averge for given start date () or end date ()
+# when given the start only, calculate tmin, tvg, and tmax for all date greater than or equal to start date
+# when given the start AND end date, calculate same for date between the start AND end date (inclusive)
+@app.route("/api/v1.0/<start>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        # Unravel results into a 1D array and convert to a list
+        temps = list(np.ravel(results))
+        return jsonify(temps)
 
-@app.route("/api/v1.0/<starts>/<end>")   
-def end_date():
-    return jsonify (end_date):
+    # calculate TMIN, TAVG, TMAX with start and stop
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps)
 
-if __name__ == "main":
-    app.run(debug=True)
+if __name__ == "__main__":
+     app.run(debug=True)
